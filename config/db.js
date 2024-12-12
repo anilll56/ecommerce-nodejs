53830bde8d2df1222d28561f54b788c6fe92d390
NumanSC <socunuman@gmail.com>
2024-12-13 01:24:15 +0300

    add mail sender

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
+const SendmailTransport = require("nodemailer/lib/sendmail-transport");
+
+const sendEmail = require("../nodeMail/nodemailer");
 require("dotenv").config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected...");
+    sendEmail();
   } catch (err) {
     console.error(err.message);
   }
